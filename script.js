4a034a4ccad3088b480839f2ed4666d0394ea23f
Алексей <Gingano@MacBook-Pro-gingano.local>
2019-05-26 11:50:12 +0300

    update

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ let click = (event) => {
 };
 
 menu.addEventListener('click', click);
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.querySelector(".btn_up").style.display = "block";
+    } else {
+        document.querySelector(".btn_up").style.display = "none";
+    }
+}
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
